12035b0ff6bbe7f07e52e89258614f090032eee7
Vincentguo <Vincentguo@anjuke.com>
2013-09-28 11:00:40 +0800

    websocket server

--- js/websocket.js
+++ js/websocket.js
@@ -3,7 +3,7 @@ var socket=null;
 var connectlimit=3;
 var util={
     init:function(){
-        this.host="127.0.0.1:1234";
+        this.host="192.168.192.191:1234";
         this.sconnect();
     },
     log:function(msg){
